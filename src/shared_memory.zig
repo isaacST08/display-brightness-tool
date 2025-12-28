59190ff1bba9fb548c002ab2d23321696943c822
isaacST08 <isaacshiellsthomas@me.com>
2025-12-28 00:18:53 -0700

    Moved SharedMemoryObject to its own file

---
+++ src/shared_memory.zig
@@ -0,0 +1,69 @@
+const std = @import("std");
+
+/// Creates, stores, and references an object of type `T` in shared memory so
+/// that it can be accessed by completely separate processes.
+pub fn SharedMemoryObject(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        shmpath: [*:0]const u8,
+        fd: c_int,
+        obj_byte_arr: []align(std.heap.page_size_min) u8,
+        obj_ptr: *T,
+        persistent: bool,
+        created_new: bool,
+
+        pub fn init(shmpath: [*:0]const u8, persistent: bool) !Self {
+
+            // Attempt to create new shared memory.
+            var fd: c_int = std.c.shm_open(shmpath, @bitCast(std.c.O{ .CREAT = true, .EXCL = true, .ACCMODE = .RDWR }), 0o666);
+
+            // Variable to record whether the shared memory was created or simply opened.
+            var created_new: bool = false;
+
+            // If the shared memory failed to open because it already exists, simply
+            // open the already existing memory.
+            if (fd == -1) {
+                const err_no: u32 = @bitCast(std.c._errno().*);
+                const err: std.posix.E = @enumFromInt(err_no);
+                switch (err) {
+                    .EXIST => {
+                        fd = std.c.shm_open(shmpath, @bitCast(std.c.O{ .ACCMODE = .RDWR }), 0o666);
+                    },
+                    else => return std.posix.unexpectedErrno(err),
+                }
+            }
+            // Otherwise, truncate memory to the appropriate length.
+            else {
+                try std.posix.ftruncate(fd, @intCast(@sizeOf(T)));
+                created_new = true;
+            }
+
+            // Get the pointer to the object.
+            const obj_byte_arr = try std.posix.mmap(null, @intCast(@sizeOf(T)), std.posix.PROT.READ | std.posix.PROT.WRITE, .{ .TYPE = .SHARED }, fd, 0);
+            const obj_ptr: *T = std.mem.bytesAsValue(T, obj_byte_arr);
+
+            // Construct self and return.
+            return Self{
+                .shmpath = shmpath,
+                .fd = fd,
+                .obj_byte_arr = obj_byte_arr,
+                .obj_ptr = obj_ptr,
+                .persistent = persistent,
+                .created_new = created_new,
+            };
+        }
+
+        pub fn deinit(self: Self) void {
+            std.posix.munmap(@alignCast(self.obj_byte_arr));
+
+            if (!self.persistent) {
+                _ = std.c.shm_unlink(self.shmpath);
+            }
+        }
+
+        pub fn sync(self: Self) void {
+            _ = std.c.msync(@ptrCast(@alignCast(self.obj_byte_arr)), @intCast(@sizeOf(T)), std.c.MSF.SYNC);
+        }
+    };
+}
